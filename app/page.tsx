58ae6a0e76e52bf35d1ad6eec9d48b8285be5f87
Ibby <33922797+SleepySwords@users.noreply.github.com>
2023-11-10 00:48:31 +1100

    feat: change colours

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-900 p-8">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
       <Navbar />
 
       <div className="relative flex grid flex-grow place-items-center">
