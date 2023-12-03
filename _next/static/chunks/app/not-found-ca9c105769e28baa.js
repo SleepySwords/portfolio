eaa479a8e56423516b6ddde3e9ac4b73a562bef5
Ibby <33922797+SleepySwords@users.noreply.github.com>
2023-12-03 22:28:54 +0700

    test: does mermaid work?

---
+++ _next/static/chunks/app/not-found-ca9c105769e28baa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9160],{93703:function(n,e,u){Promise.resolve().then(u.t.bind(u,68326,23))}},function(n){n.O(0,[8326,2971,2472,1744],function(){return n(n.s=93703)}),_N_E=n.O()}]);
