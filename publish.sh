a4bb32b5ba943cab9aa9e3c6f8ae813f7a18fea8
Ibby <33922797+SleepySwords@users.noreply.github.com>
2024-02-22 02:17:25 +1100

    fix: actually fix the publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd sleepyswords.github.io &&
-ls | grep -xv "file.txt" | xargs rm &&
+ls | grep -xv "CNAME" | xargs rm -r &&
 cd ../ &&
 yarn build &&
-cp -r out/* ./sleepyswords.github.io/
+cp -r out/* ./sleepyswords.github.io/ &&
+cd sleepyswords.github.io &&
+git stage . &&
+git commit -m "$@" &&
+git push
