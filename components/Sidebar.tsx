f5debf3ddf756bef63982cdaf34fde4c00b53c22
Ibrahim <33922797+SleepySwords@users.noreply.github.com>
2025-08-03 18:46:29 +1000

    feat: add list style to blog section to differentiate seperate blogs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,20 +8,22 @@ export default function SideBar({
 }) {
   return (
     <div>
-      <ul className="sticky top-36 mr-5">
-        <li className="mb-5 text-2xl font-bold">All blogs</li>
-        {/* HACK: The order of blogs would not change */}
-        {posts.map((post, id) => (
-          <li key={id}>
-            <Link
-              href={`/blog/${post.slug}`}
-              className="text-1xl underline transition-colors hover:text-gray-400"
-            >
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="sticky top-36 mr-5">
+        <div className="mb-5 text-2xl font-bold">All blogs</div>
+        <ul className="list-disc ml-4">
+          {/* HACK: The order of blogs would not change */}
+          {posts.map((post, id) => (
+            <li key={id}>
+              <Link
+                href={`/blog/${post.slug}`}
+                className="text-1xl underline transition-colors hover:text-gray-400"
+              >
+                {post.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
